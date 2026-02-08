074cd0d903da3df7e2ddf3eb1b001280d6d134a9
userdend <boydbayndend99@gmail.com>
2026-02-08 11:21:49 +0800

    Fix browser title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>@yield('title', 'AdminLTE v4 | Dashboard')</title>
+    <title>SentinelOps | @yield('title', 'AdminLTE v4 | Dashboard')</title>
     <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
     <meta name="color-scheme" content="light dark" />
